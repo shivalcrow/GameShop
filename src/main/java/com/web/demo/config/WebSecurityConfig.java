a8472b1c06beaed0bef002f55e77e4f7e0c0d2d5
shivalcrow <58440335+shivalcrow@users.noreply.github.com>
2021-07-27 11:38:54 +0700

    Update WebSecurityConfig.java

--- src/main/java/com/web/demo/config/WebSecurityConfig.java
+++ src/main/java/com/web/demo/config/WebSecurityConfig.java
@@ -1,13 +1,13 @@
 package com.web.demo.config;
 
-import java.net.http.HttpRequest;
 
-import javax.servlet.http.HttpServletRequest;
+
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -17,7 +17,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
+
 
 @Configuration
 @EnableWebSecurity
